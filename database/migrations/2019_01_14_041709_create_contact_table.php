10f1cb9d837363e54745e093c7184d36e3edd044
rubenson <rubenson.cristian@gmail.com>
2019-01-14 11:20:25 +0700

    commit branch ke-1

--- database/migrations/2019_01_14_041709_create_contact_table.php
+++ database/migrations/2019_01_14_041709_create_contact_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateContactTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,6 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('contact', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('email')->unique();
+            $table->string('comment');
             $table->timestamps();
         });
     }
