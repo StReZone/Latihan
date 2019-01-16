fd3cf8f5103a8d483f13248da7142ab418b15934
rubenson <rubenson.cristian@gmail.com>
2019-01-16 14:55:44 +0700

    commit ke-8, perbaiki fungsi komentar

--- resources/views/Articles/show.blade.php
+++ resources/views/Articles/show.blade.php
@@ -1,55 +1,70 @@
 @extends("template.template")
 
 @section("content")
-    <article class="row">
-        <h2>{!! $article->title !!}</h2>
-        <div>{!! $article->content !!}</div>
-        <i>By {!! $article->author !!}</i>
-    </article>
-    <div>
-        <h3><i><u>Give Comments</u></i></h3>
-        {!! Form::open(['route' => 'comments.store', 'class' => 'formhorizontal', 'role' => 'form']) !!}
+
+<article class="row">
+    <h2>{!! $article->title !!}</h2>
+    <div>{!! $article->content !!}</div>
+    <i>By {!! $article->author !!}</i>
+</article>
+<div class="row">
+    <div class="col-md-6">
+    <h3><i><u>Give Comments</u></i></h3>
+        {!! Form::open(['route' => 'comments.store', 'class' => 'form-horizontal', 'role' => 'form']) !!}
         <div class="form-group">
-            {!! Form::label('article_id', 'Title', array('class' => 'col-lg-3 control-label')) !!}
+        {!! Form::label('article_id', 'Title', array('class' => 'col-lg-3 control-label')) !!}
             <div class="col-lg-9">
                 {!! Form::text('article_id', $article->id, array('class' => 'form-control', 'readonly')) !!}
             </div>
-        <div class="clear"></div>
-    </div>
-
-    <div class="form-group">
-    {!! Form::label('content', 'Content', array('class' => 'col-lg-3 control-label')) !!}
-        <div class="col-lg-9">
-        {!! Form::textarea('content', null, array('class' => 'formcontrol', 'rows' => 10, 'autofocus' => 'true')) !!}
-        {!! $errors->first('content') !!}
-    </div>
-
-    <div class="clear"></div>
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('user', 'User', array('class' => 'col-lg-3 control-label')) !!}
-        <div class="col-lg-9">
-             {!! Form::text('user', null, array('class' => 'form-control')) !!}
         </div>
-
-    <div class="clear"></div>
-    </div>
-
+        <div class="form-group">
+        {!! Form::label('content', 'Content', array('class' => 'col-lg-3 control-label')) !!}
+            <div class="col-lg-9">
+                {!! Form::textarea('content', null, array('class' => 'form-control', 'rows' => 10, 'autofocus' => 'true')) !!}
+                {!! $errors->first('content') !!}
+            </div>
+        </div>
+        <div class="form-group">
+        {!! Form::label('user', 'User', array('class' => 'col-lg-3 control-label')) !!}
+            <div class="col-lg-9">
+                {!! Form::text('user', null, array('class' => 'form-control')) !!}
+            </div>
+        </div>
     <div class="form-group">
-    <div class="col-lg-3"></div>
-    <div class="col-lg-9">
-        {!! Form::submit('Save', array('class' => 'btn btn-primary')) !!}
+        <div class="col-lg-3"></div>
+            <div class="col-lg-9">
+            {!! Form::submit('Save', array('class' => 'btn btn-primary')) !!}
+            </div>
+        {!! Form::close() !!}
     </div>
-    <div class="clear"></div>
     </div>
-    {!! Form::close() !!}
+    <div class="col-md-6">
+    @foreach($comments as $k => $comment) 
+    @if ( $k % 2 == 0)
+        
+            <div class="bs-callout bs-callout-danger">
+                <p class="text-left">-{!! $comment->user !!}</p>
+            </div> 
+            @else
+            <div class="bs-callout bs-callout-info">
+                <p class="text-right">{!! $comment->user !!}-</p>
+            </div> 
+            @endif       
+            @if ( $k % 2 == 0)
+            <div class="bs-callout bs-callout-danger">
+                <p class="text-left">{!! $comment->content !!}</p>
+            </div>
+            @else
+            <div class="bs-callout bs-callout-info">
+                <p class="text-right">{!! $comment->content !!}</p>
+            </div>
+            @endif      
+    @endforeach
+    </div>
+
+    @stop 
 </div>
 
-@foreach($comments as $comment)
-    <div style="outline: 1px solid #74AD1B;">
-        <p>{!! $comment->content !!}</p>
-        <i>{!! $comment->user !!}</i>
-    </div>
-@endforeach
-@stop
+        
+
+
--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -18,12 +18,12 @@
     <div class="panel-body">
     @if (Session::has('error'))
         <div class="session-flash alert-danger">
-        {{Session::get('error')}}
+            {{Session::get('error')}}
         </div>
     @endif
     @if (Session::has('notice'))
         <div class="session-flash alert-info">
-        {{Session::get('notice')}}
+            {{Session::get('notice')}}
         </div>
     @endif
      @yield('content')
